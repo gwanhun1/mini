36fc65c07c9c5cac2ef1f70d9d44ccd9ecb27e76
gwanhun1 <110619143+gwanhun1@users.noreply.github.com>
2022-08-24 23:21:46 +0900

    3

--- main.js
+++ main.js
@@ -16,11 +16,13 @@ $screen.addEventListener('click',function(){
             $screen.classList.remove('ready');
             $screen.classList.add('now');
             $screen.textContent= '클릭하세요!'
-        },(Math.floor(Math.random()*1000)+2000));
+        },Math.floor(Math.random()*1000)+2000);
     }else if($screen.classList.contains('ready')){
         clearTimeout(timeoutId);
         $screen.classList.remove('ready');
-        $screen.textContent= '너무 성급하시네요!'
+        $screen.classList.add('waiting');
+        alert("너무 성급하시네요!")
+        $screen.textContent= '초록색이 되면 클릭하세요!!'
     }else if($screen.classList.contains('now')){
         endTime=new Date();
         const current = endTime - startTime;
@@ -31,9 +33,11 @@ $screen.addEventListener('click',function(){
         endTime = null;
         $screen.classList.remove('now');
         $screen.classList.add('waiting');
-        $screen.textContent= '클릭해서 시작하세요';
+        $screen.textContent= '클릭해서 시작하세요!';
         if(current <=125){alert("평균 이상입니다!!!")}
         else if(current <=325){alert("평균 입니다!!!")}
         else {alert("평균 이하입니다!!!")}
+        
     }
 });
+
