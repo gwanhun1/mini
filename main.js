0abc6feaea32f7680aeb877fac2b8dacb3c51f0f
gwanhun1 <110619143+gwanhun1@users.noreply.github.com>
2022-08-25 16:22:52 +0900

    4

--- main.js
+++ main.js
@@ -1,43 +1,57 @@
-const $screen = document.querySelector('#screen')
-const $resultAvg = document.querySelector('.resultAvg')
 
-let startTime;
-let endTime;
-let timeoutId;
-const records=[];
-
-$screen.addEventListener('click',function(){
-    if($screen.classList.contains('waiting')){
-        $screen.classList.remove('waiting')
-        $screen.classList.add('ready')
-        $screen.textContent = '초록색이 되면 클릭하세요!!'
-        timeoutId = setTimeout(function(){
-            startTime=new Date();
-            $screen.classList.remove('ready');
-            $screen.classList.add('now');
-            $screen.textContent= '클릭하세요!'
-        },Math.floor(Math.random()*1000)+2000);
-    }else if($screen.classList.contains('ready')){
-        clearTimeout(timeoutId);
+  const $screen = document.querySelector('#screen');
+  const $result = document.querySelector('#result');
+ 
+  let startTime;
+  let endTime;
+  const records = [];
+  let timeoutId;
+  $screen.addEventListener('click', function () {
+    if ($screen.classList.contains('waiting')) { // 파랑
+      $screen.classList.remove('waiting');
+      $screen.classList.add('ready');
+      $screen.textContent = '초록색이 되면 클릭하세요';
+      timeoutId = setTimeout(function () {
+        startTime = new Date();
         $screen.classList.remove('ready');
-        $screen.classList.add('waiting');
-        alert("너무 성급하시네요!")
-        $screen.textContent= '초록색이 되면 클릭하세요!!'
-    }else if($screen.classList.contains('now')){
-        endTime=new Date();
-        const current = endTime - startTime;
-        records.push(current);
-        const average = records.reduce((a,c)=>a+c)/records.length;
-        $resultAvg.textContent = `현재 ${current}ms 평균 :${average}ms`
-        startTime = null;
-        endTime = null;
-        $screen.classList.remove('now');
-        $screen.classList.add('waiting');
-        $screen.textContent= '클릭해서 시작하세요!';
-        if(current <=125){alert("평균 이상입니다!!!")}
-        else if(current <=325){alert("평균 입니다!!!")}
-        else {alert("평균 이하입니다!!!")}
-        
+        $screen.classList.add('now');
+        $screen.textContent = '클릭하세요!';
+      }, Math.floor(Math.random() * 1000) + 2000); // 2초에서 3초 사이 2000~3000 사이 수
+    } else if ($screen.classList.contains('ready')) { // 빨강
+      clearTimeout(timeoutId);
+      $screen.classList.remove('ready');
+      $screen.classList.add('waiting');
+      $screen.textContent = '너무 성급하시군요!';
+    } else if ($screen.classList.contains('now')) { // 초록
+      endTime = new Date();
+      const current = endTime - startTime;
+      records.push(current);
+      const average = records.reduce((a, c) => a + c) / records.length;
+      $result.textContent = `현재 ${current}ms, 평균: ${average}ms`;
+      const topFive = records.sort((p, c) => p - c).slice(0, 5);
+      topFive.forEach((top, index) => {
+        $result.append(
+          document.createElement('br'),
+          `${index + 1}위: ${top}ms`,
+        );
+      });
+      startTime = null;
+      endTime = null;
+      $screen.classList.remove('now');
+      $screen.classList.add('waiting');
+      $screen.textContent = '클릭해서 시작하세요';
     }
-});
-
+  });
+  new Chart(document.getElementById("line-chart"), {
+    type: 'line',
+    data: {
+      labels: [0,25,50,75,100,125,150,175,200,225,250,275,300,325,350,375,400,425,450],
+      datasets: [{ 
+          data: [0,0,0,0,5,15,100,130,50,10,10,10,6,2,0,0,0,0,0],
+          label: "person",
+          borderColor: "#black",
+          fill: false
+        }
+      ]
+    }
+  });
